bb6e75923ae9d66bef5eb1236a94a2e61dd541ae
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2020-10-16 23:43:01 +0300

    Completed a configuration webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './styles/index.sss'
-
-console.log('New app')
-async function start() {
-    return await Promise.resolve('async working')
-}
-
-start().then(console.log)
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const webpack = require('webpack')
+const {merge} = require('webpack-merge')
 
 const isProd = process.env.NODE_ENV == 'production'
 const isDev = !isProd
@@ -28,84 +28,85 @@ const jsLoaders = () => {
 console.log('prod', isProd)
 console.log('dev', isDev)
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: ['@babel/polyfill', './index.js'],
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core'),
-        }
-    },
-    optimization: {
-        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        index: 'index.html',
-        hot: isDev,
-        open: true,
-        port: 4000
-    },
-    devtool: isDev ? 'source-map' : false,
-    module: {
-        rules: [
-            {
-                test: /\.sss$/,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {importLoaders: 1},
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                parser: 'sugarss',
+module.exports = merge([
+    {
+        context: path.resolve(__dirname, 'src'),
+        mode: 'development',
+        entry: ['@babel/polyfill', './index.js'],
+        output: {
+            filename: filename('js'),
+            path: path.resolve(__dirname, 'dist')
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src/core'),
+            }
+        },
+        optimization: {
+            minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            index: 'index.html',
+            hot: isDev,
+            open: true,
+            port: 4000
+        },
+        devtool: isDev ? 'source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.sss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {importLoaders: 1},
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    parser: 'sugarss',
+                                },
                             },
                         },
-                    },
-                ],
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: filename('css'),
-            chunkFilename: '[id].css',
-        }),
-        new HtmlPlugin({
-            title: 'Excel JS',
-            filename: 'index.html',
-            favicon: path.resolve(__dirname, 'src/favicon.ico'),
-            tags: {
-                headTags: `
+                    ],
+                },
+                {
+                    test: /\.json$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: jsLoaders()
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: filename('css'),
+                chunkFilename: '[id].css',
+            }),
+            new HtmlPlugin({
+                title: 'Excel JS',
+                filename: 'index.html',
+                favicon: path.resolve(__dirname, 'src/favicon.ico'),
+                tags: {
+                    headTags: `
                     <meta charset="UTF-8">
                     `,
-                bodyTags: `
+                    bodyTags: `
                     <div id="app" class="container"></div>
                 `
-            },
-            templateContent: ({htmlWebpackPlugin}) => {
-                return `
+                },
+                templateContent: ({htmlWebpackPlugin}) => {
+                    return `
                     <!DOCTYPE html>
                     <html lang="en">
                     <head>
@@ -117,7 +118,8 @@ module.exports = {
                     </body>
                     </html>
                   `;
-            }
-        })
-    ]
-}
+                }
+            })
+        ]
+    }
+])
